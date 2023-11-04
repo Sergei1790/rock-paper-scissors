578bd090f1986baef299396dbbc0a3b4d62b7ebe
Sergei <40200071+Sergei1790@users.noreply.github.com>
2023-11-04 13:38:43 +0200

    rps

--- scripts.js
+++ scripts.js
@@ -1,29 +1,25 @@
 
-function getPlayerChoice(){
- 
-    let playerChoice = prompt('Choose: Rock, Paper or Scissors', 'Rock');
-    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
-    
-    while(playerChoice != 'Rock' && playerChoice !='Paper' && playerChoice !='Scissors'){
-        console.log('No Such Option!');
-        playerChoice = prompt('Choose: Rock, Paper or Scissors', 'Rock');
-        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
-    } 
-    return  playerChoice;
-}
-
+let playerSelection = '';
+let computerSelection = '';
+let gameScore = 0;
+let youWin = 0;
+let computerWin = 0;
 function getComputerChoice (){
     let options = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * options.length);
-    
     const computerChoice = options[randomIndex];
-
     return computerChoice;
-    
 }
 
-let youWin = 0;
-let computerWin = 0;
+let rpsButtons = document.querySelectorAll('.rps-button');
+let rps__results = document.querySelector('#rps__results');
+let rps__score = document.querySelector('#rps__score');
+rpsButtons.forEach(rpsbutton => {
+    rpsbutton.addEventListener('click', () => {
+        playerSelection = rpsbutton.value;
+        game(3);
+    });
+});
 
 function playRound (playerSelection, computerSelection){
     let winner;
@@ -44,30 +40,37 @@ function playRound (playerSelection, computerSelection){
     } else{
         winner = `It is a Draw! ${computerSelection} = ${playerSelection}`;
     }
+    let winnerWrapper = document.createElement('div');
+    winnerWrapper.classList.add('winner__result');
+    winnerWrapper.innerText = `Opponent - ${computerSelection},\n You - ${playerSelection};\n ${winner}`;
+    
+    rps__score.innerText = `Current Score: ${youWin} - ${computerWin}`
+    rps__results.appendChild(winnerWrapper);
     return winner;
 }
 
-function game(){
-    
-    for(let i = 0; i<5; i++){
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice ();
-        console.log(`You have chosen ${playerSelection}`);
-        console.log(`Computer have chosen ${computerSelection}`);
-        console.log(playRound(playerSelection, computerSelection));
+
+function game(score){
+    playRound (playerSelection, getComputerChoice());
+    if(youWin === score || computerWin === score){
+        let victory;
+        if(youWin > computerWin){
+            victory =`YOU WON! The score: ${youWin} vs ${computerWin}`
+        } else if(youWin < computerWin){
+            victory = `You lost.... The score: ${youWin} vs ${computerWin}`
+        }else{
+            victory = `It is a draw! The score: ${youWin} vs ${computerWin}`
+        }
+        let finalwinnerWrapper = document.createElement('div');
+        finalwinnerWrapper.classList.add('final-winner__result');
+        finalwinnerWrapper.innerText = victory;
+        rps__results.appendChild(finalwinnerWrapper);
+        youWin = 0;
+        computerWin = 0;
+        return victory;
     }
-    let victory;
-    if(youWin > computerWin){
-        victory =`YOU WON! The score: ${youWin} vs ${computerWin}`
-    } else if(youWin < computerWin){
-        victory = `You lost.... The score: ${youWin} vs ${computerWin}`
-    }else{
-        victory = `It is a draw! The score: ${youWin} vs ${computerWin}`
-    }
-    return victory;
-    
 }
-console.log(game());
+
 
 
 
